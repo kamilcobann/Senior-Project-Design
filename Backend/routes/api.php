c84100143ad254f9aa4879e7c7aefc3aa9a50a96
kamil coban <kamil.cobann.1@gmail.com>
2024-05-15 16:57:36 +0300

    simple realtime chat implemented

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\KanbanController;
 use App\Http\Controllers\ProjectController;
@@ -81,3 +82,5 @@ Route::controller(CommentController::class)->group(function(){
     Route::delete('/comments/{commentId}','deleteComment');
 
 });
+
+Route::post('/messages',[ChatController::class,'message']);
